53b2d9d31dca13dd62089a147a86f77e76b3939b
acornard <alexis.cornard@openio.io>
2018-05-30 11:55:39 +0200

    Revert "Add tests fof conscience persistence [WIP]"
    This reverts commit bf153ef0640d766a37c21a5384a20fcf9cfaa8de.

--- tests/functional/conscience/test_conscience_persistence.py
+++ tests/functional/conscience/test_conscience_persistence.py
@@ -14,66 +14,36 @@
 # License along with this library.
 
 import os
-import json
+from subprocess import check_call
 import time
-import subprocess
+
 from tests.utils import BaseTestCase
-from tests.utils import CODE_SRVTYPE_NOTMANAGED
+
 
 def exp(path):
     return os.path.expanduser(path)
 
+
 class TestConsciencePersistence(BaseTestCase):
 
     def setUp(self):
-        self._service("start")
         super(TestConsciencePersistence, self).setUp()
+        if not self.conf['with_persistence']:
+            self.skipTest["Conscience persistence not enabled"]
 
-    def _service(self, action, name=""):
-        if name != "":
-            name = "%s-%s" % (self.conf['namespace'], name)
-
-        subprocess.check_call(['gridinit_cmd', '-S',
+    def _service(self, name, action):
+        name = "%s-%s" % (self.conf['namespace'], name)
+        check_call(['gridinit_cmd', '-S',
                     exp('~/.oio/sds/run/gridinit.sock'), action, name])
 
-    def _start_cs(self, path, period, conf):
-        cmd = "oio-daemon"
-        opt="-O"
-        path_opt = "PersistencePath="+exp(path)
-        period_opt = "PersistencePeriod="+str(period)
-        proc = subprocess.Popen([cmd, opt, path_opt, opt,
-                                 period_opt, exp(conf)])
-        return proc
-
-    def _kill_and_watch_it_die(self, proc):
-        try:
-            proc.terminate()
-        except Exception:
-            pass
-        proc.wait()
-
-    def _register_services(self, score, srvtype):
-        srv = self._srv(srvtype)
-        srv['score'] = score
-        self._register_srv(srv)
-
-    def _check_score(self, score, srvtype):
-        resp = self.request('GET', self._url_cs('list'),
-                             params={"type":srvtype})
-        body = self.json_loads(resp.data)
-        self.assertIsInstance(body, list)
-        self.assertEqual([score], [s['score'] for s in body])
-
     def test_conscience_persistence(self):
-        self._service("stop")
-        self._service('start', 'proxy')
-        proc = self._start_cs('/tmp/pers', 1,
-                             '~/.oio/sds/conf/OPENIO-conscience-1.conf')
-        self._register_services(1, 'echo')
-        self._check_score(1, 'echo')
-        self._kill_and_watch_it_die(proc)
-        time.sleep(2)
-        proc = self._start_cs('/tmp/pers', 1,
-                              '~/.oio/sds/conf/OPENIO-conscience-1.conf')
-        self._check_score(1, 'echo')
-        self._kill_and_watch_it_die(proc)
+        self.setUp()
+        time.sleep(20)
+        self._service('@openio', 'stop')
+        self._service('@conscience @proxy', 'start')
+        services = self.conf['services']
+        for srvtype in services:
+            if 'conscience' not in srvtype:
+                srv = self.get_service(srvtype)
+                self.assertEqual(srv['status'], 'DOWN')
+                self.assertGreater(srv['score'], 0)
